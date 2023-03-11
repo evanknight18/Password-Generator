cf0397dfc6637842abd272458559c0f84bf3033e
evanknight18 <58967482+evanknight18@users.noreply.github.com>
2023-03-11 07:49:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordCharacters = {
-  upperCasecharacters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  lowerCasecharacters: "abcdefghijklmnopqrstuvwxyz",
-  numbers: "1234567890",
-  specialCharacters: "!@#$%^&*(),./<>?~`;':",
-};
 
-function generatePassword() {
-  var enterPassword = prompt("Enter your password") 
-
-  if (passwordText = passwordCharacters.upperCasecharacters) {
-    prompt("Yes");
-  } else {
-    prompt("No");
-  }
-
-};  
 
 // Write password to the #password input
 function writePassword() {
@@ -30,5 +14,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-console.log(passwordCharacters.upperCasecharacters);
