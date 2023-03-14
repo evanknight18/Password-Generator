88668e0980e35a871a133eac2e6f734264c96355
evanknight18 <58967482+evanknight18@users.noreply.github.com>
2023-03-13 21:53:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-// Define the possible characters for the password
+/* The first block defines the possible characters for the password
+  using properties in an object. Each property is a string
+  containing different sets of characters */
 var passwordCharacters = {
   upperCasecharacters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   lowerCasecharacters: "abcdefghijklmnopqrstuvwxyz",
   numbers: "0123456789",
-  specialCharacters: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+  specialCharacters: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 };
 
-// Prompt the user for the password length
+/* The next block defines the function for the password length.
+  The user will be prompted to enter the desired length and this input will parse the integer.
+  The if statement is looking to see if they entered either a non-number or a number outside the required range.
+  This process will loop until the user enters valid data.
+  */
 function getPasswordLength() {
   var passwordLength = parseInt(prompt("Enter the desired password length (must be between 8 and 128 characters)"));
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
@@ -14,9 +20,12 @@ function getPasswordLength() {
     return getPasswordLength();
   }
   return passwordLength;
-}
+};
 
-// Prompt the user for password criteria
+/* Here, the function prompts the user to select the criteria to include in the password.
+They will be given the choice of the above properties, i.e., upper case, lower case, numbers, or special characters.
+The user must select one or all of these otherwise 
+the function will circle back to the beginning of function and they must select the valid options */
 function getPasswordCriteria() {
   var useUpperCase = confirm("Include uppercase letters?");
   var useLowerCase = confirm("Include lowercase letters?");
@@ -34,9 +43,9 @@ function getPasswordCriteria() {
     useNumbers: useNumbers,
     useSpecialCharacters: useSpecialCharacters
   };
-}
+};
 
-// Generate the password based on user input
+/* This function will generate the password based on user input */
 function generatePassword() {
   var passwordLength = getPasswordLength();
   var passwordCriteria = getPasswordCriteria();
@@ -62,18 +71,16 @@ function generatePassword() {
   }
 
   return password;
-}
+};
 
-// Write password to the #password input
+/* This block of code calls to the generatePassword function and generates the password */
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+};
 
-}
-
-// Add event listener to generate button
+/* Add event listener to generate button */
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
-
